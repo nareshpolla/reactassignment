efdf869a401345e8f0ae7874eb262bbcf04ae371
nareshpolla <naresh.babu58@gmail.com>
2023-12-21 12:27:43 +0530

    initial commit

--- android/app/src/main/java/com/guzel/MainActivity.java
+++ android/app/src/main/java/com/guzel/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "guzel";
+    return "ReactNative-Assignment";
   }
 
   /**
